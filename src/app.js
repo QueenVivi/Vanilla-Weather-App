c81f705169a05ca661784b397e82bebf5e49f6ab
QueenVivi <hi@vivianxu.com>
2022-09-12 23:14:27 +1000

    ✨ Format real-time last updated time

--- src/app.js
+++ src/app.js
@@ -1,6 +1,3 @@
-// let apiKey = "3e325415f46bc35d8d6a6bc88d7c8554";
-// let city = searchInput.value;
-// let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 let searchForm = document.querySelector("#search-form");
 
 function showSearchResult() {
@@ -22,6 +19,40 @@ function showSearchResult() {
     humidityElement.innerHTML = response.data.main.humidity;
     let windElement = document.querySelector("#wind");
     windElement.innerHTML = response.data.wind.speed;
+    let localTime = document.querySelector("#local-time");
+    localTime.innerHTML = formatDate(
+      response.data.dt * 1000,
+      response.data.timezone * 1000
+    );
+
+    console.log(response.data);
+
+    function formatDate(timestamp, timezone) {
+      let time = new Date(timestamp + timezone);
+      let hours = time.getUTCHours();
+      console.log(hours);
+      if (hours < 10) {
+        hours = `0${hours}`;
+      }
+      let minutes = time.getUTCMinutes();
+      if (minutes < 10) {
+        minutes = `0${minutes}`;
+      }
+
+      let day = time.getUTCDay();
+      let weekDays = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+      let weekDay = weekDays[day];
+
+      return `Last updated at ${weekDay}, ${hours}:${minutes}.`;
+    }
   }
 }
 
