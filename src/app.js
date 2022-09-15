37d824d575be0e1e8455ddadda77a91461f92f64
QueenVivi <hi@vivianxu.com>
2022-09-15 10:37:58 +1000

    ✨ Added display forecast feature
    Display Forecast for the next 5 days about:
    .Weekday;
    .Max temperature;
    .Min temperature;
    .Weather icon.

--- src/app.js
+++ src/app.js
@@ -72,8 +72,45 @@ function getForecast(coords) {
   let lat = coords.lat;
   let lon = coords.lon;
   let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-  console.log(apiUrl);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector(".forecast");
+  let forecastHTML = `<div class = "row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+    <div class="forecast-date">${formatDay(forecastDay.dt)}</div>
+    <img
+    src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+    width="40px"
+    />
+    <div class="forecast-temp">
+    <p>
+    <span class="forecase-temp-max"> ${Math.round(
+      forecastDay.temp.max
+    )}° </span>
+    <span class="forecast-temp-min"> ${Math.round(forecastDay.temp.min)}°</span>
+    </p>
+    </div>
+    </div>`;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function showSearchResult(event) {
@@ -88,24 +125,3 @@ function showSearchResult(event) {
 }
 
 searchForm.addEventListener("submit", showSearchResult);
-
-let forecastElement = document.querySelector(".forecast");
-
-forecastHTML = `<div class = "row">`;
-forecastHTML =
-  forecastHTML +
-  `<div class="col-2">
-<div class="forecast-date">Wed</div>
-<img
-src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-width="40px"
-/>
-<div class="forecast-temp">
-<p>
-<span class="forecase-temp-max"> 18° </span>
-<span class="forecast-temp-min"> 12°</span>
-</p>
-</div>
-</div>`;
-forecastHTML = forecastHTML + `</div>`;
-forecastElement.innerHTML = forecastHTML;
